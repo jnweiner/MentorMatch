bcb61c6433c6ad17100ba5ba4651785ca4b49d4c
Jordan Weiner <weinerj8@gmail.com>
2021-01-28 21:55:48 -0800

    Implement post request functionality

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const app = express();
-const port = 3000;
+
 const db = require('../database/index.js');
+const admin = require('./controllers/Admin.js');
+const org = require('./controllers/Org.js');
+const participant = require('./controllers/Participant.js');
 
+const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../client/public')));
 
-app.use(express.static('public'));
+app.post('/api/admins', admin.add);
+app.post('/api/orgs', org.add);
+app.post('/api/participants', participant.add);
+
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
